c39efb01ee1ae074b051b49169af50b87aaba695
Mark R Russell <mrrussell@google.com>
2013-03-13 21:03:30 -0400

    Finished tests for the objects

--- Wb-deploy/src/com/wb/Release.py
+++ Wb-deploy/src/com/wb/Release.py
@@ -6,44 +6,49 @@ Created on Feb 6, 2013
 import os
 
 class Release(object):
+  '''
+  representation of a release
+  '''
+  STAGE_CONTINUOUS = 'continuous'
+  STAGE_INTEGRATION = 'integration'
+  STAGE_RELEASE = 'release'
+  stages = [STAGE_CONTINUOUS, STAGE_INTEGRATION, STAGE_RELEASE]
+
+  def __init__(self, base, tool, stage=STAGE_CONTINUOUS, version=None):
     '''
-    representation of a release
+    Constructor
     '''
-    STAGE_CONTINUOUS = 'continuous'
-    STAGE_INTEGRATION = 'integration'
-    STAGE_RELEASE = 'release'
-    stages = [STAGE_CONTINUOUS, STAGE_INTEGRATION, STAGE_RELEASE]
+    if not stage in self.stages:
+        raise Exception("stage not in ")
+    self.base_dir = base
+    self.tool = tool
+    self.version = version
+    self.stage = stage
+    self.working_dir = os.path.join(self.base_dir, tool, stage)
 
-    def __init__(self, base, tool, stage=STAGE_CONTINUOUS, version=None):
-        '''
-        Constructor
-        '''
-        if not stage in self.stages:
-            raise Exception("stage not in ")
-        self.base_dir = base
-        self.tool = tool
-        self.version = version()
-        self.stage = stage
-        self.working_dir = os.path.join(self.base_dir, tool, stage)
+  def versions(self):
+    all_versions = []
+    for name in os.listdir(self.working_dir):
+        if name != 'latest':
+            all_versions.append(name)
+    return all_versions
 
-    def versions(self):
-        all_versions = []
-        for name in os.listdir(self.working_dir):
-            if name != 'latest':
-                all_versions.append(name)
-        return all_versions
+  def latest(self):
+    latest = os.path.join(self.working_dir, 'latest');
+    if os.path.islink(latest):
+        latest = os.readlink(latest)
+    else:
+        latest = None
+    if latest is not None:
+      pos = latest.rfind(os.sep)
+      if pos >= 0:
+        latest = latest[pos:]
+    return latest
 
-    def latest(self):
-        latest = os.path.join(self.working_dir, 'latest');
-        if os.path.islink(latest):
-            return os.readlink(latest)
-        else:
-            return None
-
-    def zips(self, eclipse_versions):
-        zips = []
-        updates = os.path.join(self.working_dir, 'latest', 'update')
-        for name in os.listdir(updates):
-            if name[-3:0] == "zip":
-                zips.append(name)
-        return zips;
+  def zips(self):
+    zips = []
+    updates = os.path.join(self.working_dir, 'latest', 'update')
+    for name in os.listdir(updates):
+        if name[-3:] == "zip":
+            zips.append(name)
+    return zips;
--- Wb-deploy/src/com/wb/ReleaseTest.py
+++ Wb-deploy/src/com/wb/ReleaseTest.py
@@ -14,11 +14,11 @@ class Test(unittest.TestCase):
 
 
   def setUp(self):
-    release = Release
     self.base = 'base'
     self.tool = 'D2WBtest'
-    self.stage = release.STAGE_CONTINUOUS
-    self.temp_dir = self._createDirectories()
+    self.stage = Release.STAGE_CONTINUOUS
+    self._createDirectories()
+    self.release = Release(self.base_dir, self.tool, self.stage)
     
 
   def _createDirectories(self):
@@ -26,26 +26,48 @@ class Test(unittest.TestCase):
     tmp_base = os.path.join(tmp_dir, self.base)
     tmp_tool = os.path.join(tmp_base, self.tool)
     tmp_stage = os.path.join(tmp_tool, self.stage)
-    self.actual_path = os.path.join(tmp_stage, 'v1.2.3.23423')
+    self.version_name = 'v1.2.3.23423'
+    self.actual_path = os.path.join(tmp_stage, self.version_name)
     self.latest_path = os.path.join(tmp_stage, 'latest')
     self.update_path = os.path.join(self.actual_path, 'update')
     os.makedirs(self.update_path)
     os.symlink(self.actual_path, self.latest_path)
     
+    self.zip_count = 0;
     for i in range(1,4):
       f = open(os.path.join(self.update_path, 'zip%s.zip' % i), 'w+')
       f.close()
-
-    return tmp_dir
+      self.zip_count += 1
+    self.temp_dir = tmp_dir
+    self.base_dir = tmp_base
+    return
 
   def tearDown(self):
     shutil.rmtree(self.temp_dir)
+    self.release = None
 
 
   def testZips(self):
-    pass
+    zips = self.release.zips()
+    self.assertEqual(self.zip_count, len(zips))
+    self.assertTrue('zip1.zip' in zips)
+    self.assertTrue('zip3.zip' in zips)
+    self.assertFalse('zip4.zip' in zips)
+    self.assertFalse('zip0.zip' in zips)
 
 
+  def testLatest(self):
+    latest = self.release.latest();
+    self.assertIsNotNone(latest)
+    self.assertTrue(len(latest) > 0)
+    self.assertTrue(self.version_name, latest)
+
+
+  def testVersions(self):
+    versions = self.release.versions();
+    self.assertIsNotNone(versions)
+    self.assertEqual(1, len(versions))
+    self.assertIn(self.version_name, versions)
 if __name__ == '__main__':
   #import sys;sys.argv = ['', 'Test.testZips']
   unittest.main()
