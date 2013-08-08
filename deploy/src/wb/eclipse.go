c602c86ed81b8008f405452ffd53027eea2a0a77
Mark R Russell <mrrussell@dhcp-172-29-3-31.pit.corp.google.com>
2013-08-08 16:50:54 -0400

    added the code for some of the interface

--- deploy/src/wb/eclipse.go
+++ deploy/src/wb/eclipse.go
@@ -1,6 +1,27 @@
-package main
+package eclipse
 
 import "fmt"
+
+type eclipse interface {
+     major_minor() string
+     major_minor_service() string
+     full_version() string
+}
+
+type eclipse_data struct {
+     major int
+     minor int
+     service int
+     qualifier string
+}
+
+func (data eclipse_data) major_minor() string {
+      return fmt.Sprintf("%d.%d", data.major, data.minor)
+}
+
+func (data eclipse_data) major_minor_service() string {
+      return fmt.Sprintf("%d.%d.%d", data.major, data.minor, data.service)
+}
 
 func main() {
   fmt.Printf("Hello, world.\n")
